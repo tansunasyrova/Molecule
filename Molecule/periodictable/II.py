d95aadd85d0e3da22f178c7f9ffa88690dc38e0c
sashalands <47747263+sashalands@users.noreply.github.com>
2019-03-01 13:24:23 +0300

    Group II (#4)
    * group II added
    
    * group II all added
    
    * group II updated
    
    * commas deleted

--- Molecule/periodictable/II.py
+++ Molecule/periodictable/II.py
@@ -25,7 +25,207 @@ class GroupII:
 
 
 class Be(Element, PeriodII, GroupII):
-    pass
+    @property
+    def atomic_number(self):
+        return 4
+
+    @property
+    def atomic_mass(self):
+        return 9.012182
+
+    @property
+    def electronegativity(self):
+        return 1.57
+
+    @property
+    def common_isotope(self):
+        return 9
+
+    @property
+    def max_isotope(self):
+        return 10
+
+    @property
+    def min_isotope(self):
+        return 7
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
 
 
-__all__ = ['GroupII', 'Be']
+class Mg(Element, PeriodIII, GroupII):
+    @property
+    def atomic_number(self):
+        return 12
+
+    @property
+    def atomic_mass(self):
+        return 24.305
+
+    @property
+    def electronegativity(self):
+        return 1.31
+
+    @property
+    def common_isotope(self):
+        return 24
+
+    @property
+    def max_isotope(self):
+        return 28
+
+    @property
+    def min_isotope(self):
+        return 24
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
+
+
+class Ca(Element, PeriodIV, GroupII):
+    @property
+    def atomic_number(self):
+        return 20
+
+    @property
+    def atomic_mass(self):
+        return 40.078
+
+    @property
+    def electronegativity(self):
+        return 1.00
+
+    @property
+    def common_isotope(self):
+        return 40
+
+    @property
+    def max_isotope(self):
+        return 48
+
+    @property
+    def min_isotope(self):
+        return 40
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
+
+
+class Sr(Element, PeriodV, GroupII):
+    @property
+    def atomic_number(self):
+        return 38
+
+    @property
+    def atomic_mass(self):
+        return 87.62
+
+    @property
+    def electronegativity(self):
+        return 0.95
+
+    @property
+    def common_isotope(self):
+        return 88
+
+    @property
+    def max_isotope(self):
+        return 90
+
+    @property
+    def min_isotope(self):
+        return 82
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
+
+
+class Ba(Element, PeriodVI, GroupII):
+    @property
+    def atomic_number(self):
+        return 56
+
+    @property
+    def atomic_mass(self):
+        return 137.327
+
+    @property
+    def electronegativity(self):
+        return 0.89
+
+    @property
+    def common_isotope(self):
+        return 138
+
+    @property
+    def max_isotope(self):
+        return 138
+
+    @property
+    def min_isotope(self):
+        return 130
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
+
+
+class Ra(Element, PeriodVII, GroupII):
+    @property
+    def atomic_number(self):
+        return 88
+
+    @property
+    def atomic_mass(self):
+        return 226.0254
+
+    @property
+    def electronegativity(self):
+        return 0.9
+
+    @property
+    def common_isotope(self):
+        return 226
+
+    @property
+    def max_isotope(self):
+        return 228
+
+    @property
+    def min_isotope(self):
+        return 223
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1)
+
+    @property
+    def valences_exeptions(self):
+        return ()
+
+
+__all__ = ['GroupII', 'Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra']
