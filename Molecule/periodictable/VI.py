b1b64df7aa4731b2f8ab8d1245fcc3f2343c104e
Tagir <tagirshin@gmail.com>
2019-03-19 13:08:07 +0300

    Groups files (#18)
    * Added group files
    
    * Update XIV.py
    
    * Update XII.py

---
+++ Molecule/periodictable/VI.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2019 Ramil Nugmanov <stsouko@live.ru>
+#  Copyright 2019 Tagir Akhmetshin <tagirshin@gmail.com>
+#  This file is part of Molecule.
+#
+#  Molecule is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, see <https://www.gnu.org/licenses/>.
+#
+from .element import Element
+from .periods import *
+
+
+class GroupVI:
+    pass
+
+
+__all__ = ['GroupVI']
