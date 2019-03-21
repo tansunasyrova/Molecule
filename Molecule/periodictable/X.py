c1e3842ea8834a7819c2fe07eb1bfb087e8f1686
dayanabashirova <47747228+dayanabashirova@users.noreply.github.com>
2019-03-21 23:52:21 +0300

    X (#41)
    * X group added
    
    * X group added; copyright fixed
    
    * X group exceptions updated; Ni multiplicity fixed

--- Molecule/periodictable/X.py
+++ Molecule/periodictable/X.py
@@ -2,6 +2,7 @@
 #
 #  Copyright 2019 Ramil Nugmanov <stsouko@live.ru>
 #  Copyright 2019 Tagir Akhmetshin <tagirshin@gmail.com>
+#  Copyright 2019 Dayana Bashirova <dayana.bashirova@yandex.ru>
 #  This file is part of Molecule.
 #
 #  Molecule is free software; you can redistribute it and/or modify
@@ -25,4 +26,182 @@ class GroupX:
     pass
 
 
-__all__ = ['GroupX']
+class Ni(Element, PeriodIV, GroupX):
+    @property
+    def atomic_number(self):
+        return 28
+
+    @property
+    def atomic_mass(self):
+        return 58.6934
+
+    @property
+    def electronegativity(self):
+        return 1.91
+
+    @property
+    def common_isotope(self):
+        return 58
+
+    @property
+    def max_isotope(self):
+        return 64
+
+    @property
+    def min_isotope(self):
+        return 56
+
+    @property
+    def common_valences(self):
+        return (0, 3), (2, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((-2, 1, ((1, 'Ni'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Ni2(CO)6]2-
+                (-2, 1, ((1, 'C'),(1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Ni(CO)6]2-
+                (-2, 1, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Ni(OH)4]2-, [Ni(NO3)4]
+                (-1, 1, ((1, 'O'), (1, 'O'), (1, 'O'))),  # [Ni(NO3)3]-
+                (-4, 1, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Ni(NO2)6]4-
+                (-2, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Ni(CN)4]2-
+                (-3, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Ni(CN)5]3-
+                (-4, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Ni(NCS)6]4-
+                (-4, 1, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Ni(SCN)6]4-
+                (-2, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Ni(NCS)4]2-
+                (-2, 1, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Ni(SCN)4]2-
+                (-2, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # [NiF4]2-
+                (-2, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [NiCl4]2-
+                (-2, 1, ((1, 'Br'), (1, 'Br'), (1, 'Br'), (1, 'Br'))),  # [NiBr4]2-
+                (-3, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))), # [NiCl5]3-
+                (0, 1, ((2, 'O'), (1, 'O')))) # Ni2O3
+
+
+class Pd(Element, PeriodV, GroupX):
+    @property
+    def atomic_number(self):
+        return 46
+
+    @property
+    def atomic_mass(self):
+        return 106.42
+
+    @property
+    def electronegativity(self):
+        return 2.20
+
+    @property
+    def common_isotope(self):
+        return 106
+
+    @property
+    def max_isotope(self):
+        return 110
+
+    @property
+    def min_isotope(self):
+        return 102
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1), (4, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((-2, 1, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Pd(OH)4]2-
+                (-2, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Pd(CN)4]2-
+                (-2, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Pd(NCS)4]2-
+                (-2, 1, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Pd(SCN)4]2-
+                (-2, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # [PdF4]2-
+                (-2, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [PdCl4]2-
+                (-2, 1, ((1, 'Br'), (1, 'Br'), (1, 'Br'), (1, 'Br'))),  # [PdBr4]2-
+                (-2, 1, ((1, 'I'), (1, 'I'), (1, 'I'), (1, 'I'))))  # [PdI4]2-
+
+
+class Pt(Element, PeriodVI, GroupX):
+    @property
+    def atomic_number(self):
+        return 78
+
+    @property
+    def atomic_mass(self):
+        return 195.084
+
+    @property
+    def electronegativity(self):
+        return 2.28
+
+    @property
+    def common_isotope(self):
+        return 195
+
+    @property
+    def max_isotope(self):
+        return 198
+
+    @property
+    def min_isotope(self):
+        return 190
+
+    @property
+    def common_valences(self):
+        return (0, 1), (2, 1), (4, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((-2, 1, ((1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'), (1, 'O'))),  # [Pt(OH)6]2-
+                (-2, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Pt(CN)4]2-
+                (-2, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'))),  # [Pt(CN)6]2-
+                (-2, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Pt(NCS)4]2-
+                (-2, 1, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Pt(SCN)4]2-
+                (-2, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'))),  # [Pt(NCS)6]4-
+                (-2, 1, ((1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'), (1, 'S'))),  # [Pt(SCN)6]4-
+                (-2, 1, ((1, 'H'), (1, 'H'), (1, 'H'), (1, 'H'))),  # [PtH4]2-
+                (-2, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [PtCl4]2-
+                (-1, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'N'))),  # [Pt(NH3)Cl5]-
+                (-1, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'N'))),  # [Pt(NH3)Cl3]-
+                (-2, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # [PtCl6]2-
+                (-2, 1, ((1, 'I'), (1, 'I'), (1, 'I'), (1, 'I'), (1, 'I'), (1, 'I'))),  # [PdI6]2-
+                (0, 1, ((1, 'Cl'), (1, 'Cl'), (1, 'Cl'))),  # PtCl3
+                (0, 1, ((1, 'Br'),(1, 'Br'), (1, 'Br'))),  # PtBr3
+                (0, 1, ((1, 'I'), (1, 'I'), (1, 'I'))),  # PtI3
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'))),  # PtF3
+                (0, 1, ((2, 'O'), (1, 'O'))),  # Pt2O3
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # PtF5
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),  # PtF6
+                (0, 1, ((2, 'O'), (2, 'O'), (2, 'O'))))  # PtO3
+
+
+class Ds(Element, PeriodVII, GroupX):
+    @property
+    def atomic_number(self):
+        return 110
+
+    @property
+    def atomic_mass(self):
+        return 281
+
+    @property
+    def electronegativity(self):
+        return None
+
+    @property
+    def common_isotope(self):
+        return 281
+
+    @property
+    def max_isotope(self):
+        return 281
+
+    @property
+    def min_isotope(self):
+        return 271
+
+    @property
+    def common_valences(self):
+        return ()
+
+    @property
+    def valences_exceptions(self):
+        return ()
+
+
+__all__ = ['GroupX', 'Ni', 'Pd', 'Pt', 'Ds']
