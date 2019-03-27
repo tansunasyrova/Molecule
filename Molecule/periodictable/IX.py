77e775af0dd3426a13a120814a4a4b44f368c3de
tansunasyrova <tansu.nasyrova@gmail.com>
2019-03-27 18:15:53 +0300

    IX group were fixed (Co, Rh, Ir) 27_03

--- Molecule/periodictable/IX.py
+++ Molecule/periodictable/IX.py
@@ -2,6 +2,7 @@
 #
 #  Copyright 2019 Ramil Nugmanov <stsouko@live.ru>
 #  Copyright 2019 Tagir Akhmetshin <tagirshin@gmail.com>
+#  Copyright 2019 Tansu Nasyrova <tansu.nasurova@gmail.com>
 #  This file is part of Molecule.
 #
 #  Molecule is free software; you can redistribute it and/or modify
@@ -25,4 +26,124 @@ class GroupIX:
     pass
 
 
-__all__ = ['GroupIX']
+class Co(Element, PeriodIV, GroupIX):
+    @property
+    def atomic_number(self):
+        return 27
+
+    @property
+    def atomic_mass(self):
+        return 58.9332
+
+    @property
+    def electronegativity(self):
+        return 1.88
+
+    @property
+    def common_isotope(self):
+        return 59
+
+    @property
+    def max_isotope(self):
+        return 60
+
+    @property
+    def min_isotope(self):
+        return 56
+
+    @property
+    def common_valences(self):
+        return (2, 1), (3, 1)
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 1, (1, 'H')),
+                (0, 1, (2, 'O'), (2, 'O')),
+                (0, 1, (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F')),
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'Co'))), # Октакарбонилдикобальт
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'H'))), # Тетракарбонилкобальтовая кислота
+                (1, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'C'))), # Витамин B12
+                (1, 1, ((1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'N'), (1, 'O')))) # Гидроксокобаламин
+                # Возникли вопросы насчет арсенидов, фосфидов, нитридов, силицидов кобальта.
+                # Их много и непонятно, какие там валентности.
+
+
+class Rh(Element, PeriodV, GroupIX):
+    @property
+    def atomic_number(self):
+        return 45
+
+    @property
+    def atomic_mass(self):
+        return 102.9055
+
+    @property
+    def electronegativity(self):
+        return 2.28
+
+    @property
+    def common_isotope(self):
+        return 103
+
+    @property
+    def max_isotope(self):
+        return 103
+
+    @property
+    def min_isotope(self):
+        return 99
+
+    @property
+    def common_valences(self):
+        return (1, 2), (1, 3), (1, 4),
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'H'))), # Тетракарбонилродиевая кислота
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'Rh'))), # Октакарбонил родия
+                (0, 1, ((1, 'P'), (1, 'P'), (1, 'P'), (1, 'C'), (1, 'H'))), # Гидридотрис(трифенилфосфин)карбонилродий
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))),
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'Rh'), (1, 'Rh'), (1, 'Rh'))), # Додекакарбонил родия
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'Rh'), (1, 'Rh'), (1, 'Rh'))), # Додекакарбонил
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'Rh'), (1, 'Rh'), (1, 'Rh'), (1, 'Rh')))) # Гексадекакарбонил
+
+
+class Ir(Element, PeriodVI, GroupIX):
+    @property
+    def atomic_number(self):
+        return 77
+
+    @property
+    def atomic_mass(self):
+        return 192.217
+
+    @property
+    def electronegativity(self):
+        return 2.20
+
+    @property
+    def common_isotope(self):
+        return 193
+
+    @property
+    def max_isotope(self):
+        return 194
+
+    @property
+    def min_isotope(self):
+        return 189
+
+    @property
+    def common_valences(self):
+        return (1, 2), (1, 3), (1, 4), (1, 6),
+
+    @property
+    def valences_exceptions(self):
+        return ((0, 1, (1, 'Br')),
+                (0, 1, (1, 'I')),
+                (0, 1, ((1, 'C'), (1, 'C'), (1, 'C'), (1, 'C'), (1, 'Ir'))), # Октакарбонил иридия
+                (0, 1, ((1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'), (1, 'F'))))
+
+
+__all__ = ['GroupIX', 'Co', 'Rh', 'Ir']
